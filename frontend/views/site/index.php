1a63a79bf59b34001ded16d869f8b07b6852415d
jeyfost <foster_andrew@tut.by>
2016-02-09 18:14:17 +0300

    Main page with main block

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,49 +5,13 @@
 $this->title = 'Джей Фост | Стихи с запахом моря';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <div class="main-block">
+        <img src="<?= Yii::$app->homeUrl; ?>/img/jeyfost.png" />
+        <div class="main-block-text">
+            <div class="main-text">
+                <h3><b>Стихи с запахом моря</b></h3>
+                <p style="padding: 5px;">если бы можно было забрать с собой волны,<br />такие же, как в книгах жюль верна,<br />закат и причал —<br />я бы, наверное,<br />не стал забирать,<br />а просто бы не уезжал.</p>
             </div>
         </div>
-
     </div>
 </div>
--- frontend/web/js/basic.js
+++ frontend/web/js/basic.js
@@ -1,3 +1,15 @@
-$(document).ready(function () {
+$(window).load(function () {
+    mainTextPos();
+});
 
-})
+$(window).resize(function () {
+    mainTextPos();
+});
+
+function mainTextPos() {
+    $('.main-text').offset({top: $('.main-block img').offset().top});
+
+    var tOffset = parseInt($('.main-block img').height() / 2 - $('.main-text').height() / 2 + $('.main-block').offset().top);
+
+    $('.main-text').offset({top: tOffset});
+}
